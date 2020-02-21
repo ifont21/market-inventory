049cfae9d7c9e9bb4ea130066b104037cb040e1e
Ignacio Fontalvo <ifontalvo@MacBook-Pro.local>
2020-02-21 08:59:46 -0500

    Updates

---
+++ precache-manifest.51c10276e983f39b85802b9920ead71f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ec9f3eaff2e0035c0f604391be8634",
+    "url": "/market-inventory/index.html"
+  },
+  {
+    "revision": "bc2bd087995a8d91379c",
+    "url": "/market-inventory/static/css/main.b377dbc5.chunk.css"
+  },
+  {
+    "revision": "56951e551b734448ff45",
+    "url": "/market-inventory/static/js/2.0d24ed58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/market-inventory/static/js/2.0d24ed58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc2bd087995a8d91379c",
+    "url": "/market-inventory/static/js/main.931698d1.chunk.js"
+  },
+  {
+    "revision": "95a4f3c1605bf3dc5682",
+    "url": "/market-inventory/static/js/runtime-main.7125c3af.js"
+  }
+]);
